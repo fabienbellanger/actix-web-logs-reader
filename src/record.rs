36104eb0fbcece89995bbf2419d32c6861fae201
Fabien Bellanger <valentil@gmail.com>
2021-06-08 00:21:53 +0200

    Avancement

--- src/record.rs
+++ src/record.rs
@@ -1,9 +1,11 @@
 use chrono::{DateTime, SecondsFormat, Utc};
 use colored::Colorize;
 use serde::Deserialize;
+use regex::Regex;
 
 pub const LEVELS: [&str; 6] = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"];
 
+// TODO: Replace String to &str?
 #[derive(Deserialize, Debug)]
 pub struct LogRecord {
     pub time: DateTime<Utc>,
@@ -15,10 +17,10 @@ impl LogRecord {
     /// Format a record
     pub fn format(&self) -> String {
         format!(
-            "{} {:>5}: {}\n",
+            "{} {}: {}\n",
             self.time.to_rfc3339_opts(SecondsFormat::Secs, true).bright_black(),
-            format_level(&self.level),
-            format_msg(&self.msg),
+            self.format_level(),
+            self.format_msg(),
         )
     }
 
@@ -29,20 +31,76 @@ impl LogRecord {
 
         level_index >= target_level_index
     }
+
+    /// Format record level
+    fn format_level(&self) -> String {
+        match LEVELS.iter().position(|&r| r == self.level).unwrap_or(0) {
+            0 => "TRACE".magenta(),
+            1 => "DEBUG".blue(),
+            2 => " INFO".green(),
+            3 => " WARN".yellow(),
+            4 => "ERROR".red(),
+            _ => "FATAL".red(),
+        }.to_string()
+    }
+
+    /// Format message
+    fn format_msg(&self) -> String {
+        let re = Regex::new(r#"^request_id=(.*), client_ip_address=(.*), request_path="(.*)", status_code=(.*), elapsed_seconds=(.*), user_agent="(.*)"$"#);
+
+        let mut access_log = AccessLogRecord::default();
+        if re.is_ok() {
+            for cap in re.unwrap().captures_iter(&self.msg) {
+                access_log.request_id = cap.get(1).map_or("".to_owned(), |m| m.as_str().to_owned());
+                access_log.client_ip_address = cap.get(2).map_or("".to_owned(), |m| m.as_str().to_owned());
+                access_log.request_path = cap.get(3).map_or("".to_owned(), |m| m.as_str().to_owned());
+                access_log.status_code = cap.get(4).map_or(0, |m| m.as_str().parse::<u16>().unwrap());
+                access_log.elapsed_seconds = cap.get(5).map_or(0.0, |m| m.as_str().parse::<f32>().unwrap());
+                access_log.user_agent = cap.get(6).map_or("".to_owned(), |m| m.as_str().to_owned());
+            }
+        }
+
+        if access_log.is_valid() {
+            format!("{} | {} | {} | {: >10}s | {} | {}",
+                access_log.status_code.to_string().on_green().black(), // TODO: Customize color
+                access_log.request_path, // TODO: Customize method color
+                access_log.request_id,
+                access_log.elapsed_seconds,
+                access_log.client_ip_address,
+                access_log.user_agent,
+            )
+        } else {
+            self.msg.to_string()
+        }
+    }
 }
 
-fn format_level(level: &str) -> String {
-    // Possibilité d'utiliser truecolor(u8,u8,u8)
-    match LEVELS.iter().position(|&r| r == level).unwrap_or(0) {
-        0 => level.to_string().purple().bold(),
-        1 => level.to_string().blue().bold(),
-        2 => level.to_string().green().bold(),
-        3 => level.to_string().yellow().bold(),
-        4 => level.to_string().red().bold(),
-        _ => level.to_string().reversed().bold(),
-    }.to_string()
+// TODO: Only &str?
+#[derive(Debug)]
+struct AccessLogRecord {
+    request_id: String,
+    client_ip_address: String,
+    request_path: String,
+    status_code: u16,
+    elapsed_seconds: f32,
+    user_agent: String,
 }
 
-fn format_msg(msg: &str) -> String {
-    msg.to_string()
+impl AccessLogRecord {
+    fn is_valid(&self) -> bool {
+        self.status_code >= 100 && self.status_code <= 599
+    }
+}
+
+impl Default for AccessLogRecord {
+    fn default() -> Self {
+        Self {
+            request_id: String::from(""),
+            client_ip_address: String::from(""),
+            request_path: String::from(""),
+            status_code: 0,
+            elapsed_seconds: 0.0,
+            user_agent: String::from(""),
+        }
+    }
 }
--- src/source.rs
+++ src/source.rs
@@ -5,7 +5,6 @@ pub fn process_stdin(level_filter: String, strict: bool) {
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        // println!("line: {}", line);
         match serde_json::from_str::<LogRecord>(&line) {
             Ok(r) => {
                 if r.display_record(&level_filter.to_uppercase()) {
